e4f991c22b21c88e9e38fc65b142d198dbf86cfe
craigpearce5 <Craigpearce5@gmail.com>
2015-07-20 22:06:51 -0600

    Initial Commit

---
+++ DaycareIQ Child Care Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaycareIQ Child Care Locator
+//
+//  Created by Craig on 2015-07-20.
+//  Copyright (c) 2015 DaycareIQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
